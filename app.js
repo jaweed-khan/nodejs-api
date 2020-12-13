5c7185c82fa09fd2a48ca05f63f47a07fcad6489
Jaweed Khan <mr.jaweedkhan@gmail.com>
2020-12-13 15:10:59 +0530

    aded test api

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get("/api", (req,res) => {
+    res.json({
+        success: 1,
+        message: "This rest api is working"
+    });
+});
+
+app.listen(3000, () => {
+    console.log('server up and running');
+});
